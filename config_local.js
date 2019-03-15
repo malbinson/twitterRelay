8d512d1a13a8dfd12080bf571e5f7ede82cd1449
malbinson <mattalbinson@berkeley.net>
2019-03-15 11:43:47 -0700

    next

---
+++ config_local.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+
+
+  'twitterAuth' : {
+      'consumerKey' : 'tdoGiseFHuXlkUWNw28Ch6E3h',
+      'consumerSecret' : 'OTUnHIMVfZ5LkMOFOv6guSxM0NmnNeT5ap9OShFHPDgq5n2r1e',
+      'userKey' : '329966411-4yYK33VeqUom3FMJUSlOX9OsZDLanxfiYpmne2UZ',
+      'userSecret' : 'X1JawQHiLCqgp0r4vjCfIO4WVd1GVfWUZYodw4vHp4ZHB'
+  }
+
+
+}
